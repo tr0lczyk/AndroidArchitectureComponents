6705ab28bc53d669a7c9574436df390f0eaf55c8
Mateusz <30321069+tr0lczyk@users.noreply.github.com>
2018-10-29 13:49:55 +0100

    repo initialize

---
+++ app/src/main/java/com/olczyk/android/androidarchitectureapp/MainActivity.java
@@ -0,0 +1,11 @@
+package com.olczyk.android.androidarchitectureapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+}
